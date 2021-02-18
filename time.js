f641cd3c990ff5af745605609e74ce296575ca40
“benzkittisak” <6206021621082@fitm.kmutnb.ac.th>
2021-02-18 13:54:36 +0700

    แก้บัค 24 ชั่วโมงให้ขึ้น 00

--- time.js
+++ time.js
@@ -1,10 +1,10 @@
 function digitalClock(time){
-    let hour = (Math.floor(time / 3600) < 10) ? `0${Math.floor(time / 3600)}` : Math.floor(time / 3600) ;
+    let hour = (Math.floor(time / 3600) < 10) ? `0${Math.floor(time / 3600)}` : Math.floor(time / 3600);
     time -= hour * 3600;
     let mins = (Math.floor(time / 60) < 10) ? `0${Math.floor(time / 60)}` : Math.floor(time / 60) ;
     time -= mins * 60 ;
     let secs = (time < 10) ? `0${time}` : time ;
-    
+    if(hour === 24) hour = '00';
     return `${hour}:${mins}:${secs}`;
 
 }
