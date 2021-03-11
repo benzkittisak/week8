8339efb83f006df149820b69e44cc0ace568ba25
Kittisak077 <6206021621082@fitm.kmutnb.ac.th>
2021-03-11 09:20:37 +0700

    update remove.js

--- remove.js
+++ remove.js
@@ -1,10 +1,14 @@
 function removeDups(data){
+    /*
     let result = [];
     
     data.forEach(element => {
        if(!result.includes(element)) result.push(element);
     });
     return result;
+    */
+   
+   return Array.from(new Set(data));
 }
 
 console.log(removeDups([1,0,1,0]))
